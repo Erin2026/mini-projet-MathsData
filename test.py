f5dc446c96457ce22bf6b14797078be9c3bf9bdd
Erin2026 <Perrine.wurtz@gmail.com>
2025-01-31 15:44:18 +0100

    poubelle

--- test.py
+++ test.py
@@ -2,36 +2,55 @@ import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 from descente_stochastique_gpt import GradientDescent
 
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+def neurone(thetai,x):                  #fonction de prédiction d'un neurone
+    y = 0
+    for i in range(len(x)):
+        y += np.dot(thetai[i],x[i])
+    return y
+
+def totalneurone(theta,x):             #division par la somme des exp de chaque neurone
+    div = np.array([0]*len(theta))
+    for i in range(len(theta)):
+        div[i] = neurone(theta[i],x)
+    return np.sum(div)
+
+def softmax(theta,z):
+    result = np.array([0]*len(theta))
+    totalneurone = totalneurone(theta,z)
+    for i in range(len(theta)):
+        y = neurone(theta[i],z)
+        result[i] = np.exp(y)/totalneurone
+    return z
+
+def errorSoftmax(theta,x,y):
+    error = np.array([0]*len(theta[0]))
+    for i in range(len(theta)):
+        error[i] = -y[i]*np.log(softmax(theta,x))
+    return error
+
+def gradientErrorSoftmax(theta,x,y):
+    gradError = np.array([0]*len(theta[0]))
+    for i in range(len(theta)):
+        gradError[i] = x*(softmax(theta,x)-y[i])
+    return gradError
 
 
-def cost_gradient(theta, X, y):
-    m = len(y)
-    predictions = sigmoid(X @ theta)
-    errors = predictions - y
-    return (1 / m) * (X.T @ errors)
-
-
-def predict(theta, X):
-    return (sigmoid(X @ theta) >= 0.5).astype(int)
-
-
-# Charger le dataset
 digits = load_digits()
-X = digits.data / 16.0
-y = (digits.target == 1).astype(int)  # Classification binaire : "1" vs "non-1"
+print(digits.data)
+scaler = StandardScaler()
+X = scaler.fit_transform(digits.data)
+print(X.shape)
+
+y = digits.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Ajouter le biais
 X_train_bias = np.c_[np.ones(X_train.shape[0]), X_train]
 X_test_bias = np.c_[np.ones(X_test.shape[0]), X_test]
 
-
-# Initialiser les poids
-theta_init = np.zeros(X_train_bias.shape[1])
-print(X_train_bias.shape[1])
+theta_init = np.array([np.zeros(X_train_bias.shape[1])]*len(digits.target_names))
+print(theta_init.shape)
